a28feb74923593852cb56fa0aa7f217975bc6c16
russel_chua <russel.chua@iisigroup.com>
2021-10-18 16:02:53 +0800

    init Spring Boot project

---
+++ src/main/java/com/whattoeatforlunch/app/WhatToEatForLunchApplication.java
@@ -0,0 +1,13 @@
+package com.whattoeatforlunch.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatToEatForLunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatToEatForLunchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whattoeatforlunch/app/WhatToEatForLunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whattoeatforlunch.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatToEatForLunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
